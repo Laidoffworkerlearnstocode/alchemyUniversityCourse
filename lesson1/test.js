503d92c5e077c2b1489d7c89b2ebbf0cfa4c6fe9
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-06-17 15:04:44 +0800

    finish stage 1

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,10 +1,14 @@
-const { sha256 } = require("ethereum-cryptography/sha256")
-const { toHex, utf8ToHex } = require("ethereum-cryptography/utils")
+const {assert} = require('chai');
+const findColor = require('./index');
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
 
-const COLORS = ["red", "green", "blue", "yellow", "pink", "orange"]
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
-function findColor(hash) {
-    
-}
-
-module.exports = findColor;
+describe('findColor', () => {
+    COLORS.forEach((color) => {
+        it(`should work for ${color}`, () => {
+            assert.equal(findColor(sha256(utf8ToBytes(color))), color);
+        });
+    });
+});
