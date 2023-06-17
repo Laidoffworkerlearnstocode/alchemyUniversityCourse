a271b00b75019cff6081988bf871a36e90f427d2
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-06-17 13:49:34 +0800

    project init

---
+++ lesson1/test.js
@@ -0,0 +1,10 @@
+const { sha256 } = require("ethereum-cryptography/sha256")
+const { toHex, utf8ToHex } = require("ethereum-cryptography/utils")
+
+const COLORS = ["red", "green", "blue", "yellow", "pink", "orange"]
+
+function findColor(hash) {
+    
+}
+
+module.exports = findColor;
